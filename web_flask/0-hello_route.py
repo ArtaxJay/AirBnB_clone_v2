c09bddaa9feae6cd2820a3c530e55e87920e4e30
ArtaxJay <okjaceauthor@gmail.com>
2024-02-16 09:33:01 +0000

    Add 0-hell*

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 # This is the app.py module for this flask project
 
-# Import the flask class from its module
 from flask import Flask
+# Import the flask class from its module
 
 
-# Instantiate/create a flask app
 app = Flask(__name__)
+# Instantiate/create a flask app
 
 
-# Listen/link to the home url
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Listen/link to the home url """
     return 'Hello HBNB!'
 
 
-# Disallow importing and running app.py as a module
 if __name__ == '__main__':
+    """ Disallow importing and running app.py as a module """
     app.run(host='0.0.0.0', port=5000)
