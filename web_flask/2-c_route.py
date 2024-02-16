63e5d80d414402dbde0f903993f962ba3aeb0e46
ArtaxJay <okjaceauthor@gmail.com>
2024-02-16 12:03:14 +0000

    Add 2-*

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is the app.py module for this flask project
+# Import the flask class from its module
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Listen/link to the home url """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Listen for the /hbnb url. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def dynamic_url(text):
+    """ Listen for /c/dynamic url. """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
